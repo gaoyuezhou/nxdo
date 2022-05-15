ff1d7de1b693ea53ed0c8afb09696bb96735f9fc
gaoyuezhou <zhougy_99@berkeley.edu>
2022-05-15 16:24:00 -0400

    update bridge config & scenario files

--- grl/rl_apps/scenarios/catalog/bridge_psro_scenarios.py
+++ grl/rl_apps/scenarios/catalog/bridge_psro_scenarios.py
@@ -10,6 +10,7 @@ from grl.rllib_tools.modified_policies.simple_q_torch_policy import SimpleQTorch
 # from grl.envs.tiny_bridge_2p_multi_agent_env  import TinyBridge2pMultiAgentEnv
 # from grl.rl_apps.tiny_bridge_2p_mappo import CCTrainer, CCPPOTorchPolicy
 from grl.rl_apps.tiny_bridge_4p_mappo import CCTrainer_4P, CCPPOTorchPolicy_4P
+from grl.envs.bridge_4p_multi_agent_env import BridgeMultiAgentEnv
 
 scenario_catalog.add(PSROScenario(
     name="tiny_bridge_4p_psro",
@@ -36,6 +37,43 @@ scenario_catalog.add(PSROScenario(
     p2sro_sync_with_payoff_table_every_n_episodes=None,
     single_agent_symmetric_game=False,
     get_trainer_config=psro_tiny_bridge_ccppo_params,
+    # psro_get_stopping_condition= lambda: StopImmediately(),
+    psro_get_stopping_condition=lambda: EpisodesSingleBRRewardPlateauStoppingCondition(
+        br_policy_id="best_response",
+        dont_check_plateau_before_n_episodes=int(2e4),
+        check_plateau_every_n_episodes=int(2e4),
+        minimum_reward_improvement_otherwise_plateaued=0.01,
+        max_train_episodes=int(1e5),
+    ),
+    calc_exploitability_for_openspiel_env=False,
+))
+
+
+scenario_catalog.add(PSROScenario(
+    name="bridge_psro",
+    ray_cluster_cpus=default_if_creating_ray_head(default=64),
+    ray_cluster_gpus=default_if_creating_ray_head(default=0),
+    ray_object_store_memory_cap_gigabytes=1,
+    env_class=BridgeMultiAgentEnv,
+    env_config={
+        "version": "bridge",
+        "fixed_players": True,
+    },
+    mix_metanash_with_uniform_dist_coeff=0.0,
+    allow_stochastic_best_responses=False,
+    trainer_class=CCTrainer_4P,
+    policy_classes={
+        "metanash": CCPPOTorchPolicy_4P,
+        "best_response": CCPPOTorchPolicy_4P,
+        "eval": CCPPOTorchPolicy_4P,
+    },
+    num_eval_workers=8,
+    games_per_payoff_eval=2000,
+    p2sro=False,
+    p2sro_payoff_table_exponential_avg_coeff=None,
+    p2sro_sync_with_payoff_table_every_n_episodes=None,
+    single_agent_symmetric_game=False,
+    get_trainer_config=psro_bridge_ccppo_params,
     # psro_get_stopping_condition= lambda: StopImmediately(),
     psro_get_stopping_condition=lambda: EpisodesSingleBRRewardPlateauStoppingCondition(
         br_policy_id="best_response",
--- grl/rl_apps/scenarios/trainer_configs/bridge_psro_configs.py
+++ grl/rl_apps/scenarios/trainer_configs/bridge_psro_configs.py
@@ -1,37 +1,96 @@
 import os
 from typing import Dict, Any
+
 from ray.rllib.env import MultiAgentEnv
 from ray.rllib.models import MODEL_DEFAULTS
 from ray.rllib.models.torch.torch_action_dist import TorchBeta
 from ray.rllib.utils import merge_dicts
 from ray.tune.registry import RLLIB_ACTION_DIST, _global_registry
+
 from grl.rl_apps.scenarios.trainer_configs.defaults import GRL_DEFAULT_OPENSPIEL_POKER_DQN_PARAMS, \
     GRL_DEFAULT_POKER_PPO_PARAMS
 from grl.rllib_tools.action_dists import TorchGaussianSquashedGaussian
 from grl.rllib_tools.models.valid_actions_fcnet import get_valid_action_fcn_class_for_env
 from grl.rllib_tools.valid_actions_epsilon_greedy import ValidActionsEpsilonGreedy
+
 from grl.envs.tiny_bridge_2p_multi_agent_env  import TinyBridge2pMultiAgentEnv
 from grl.envs.tiny_bridge_4p_multi_agent_env  import TinyBridge4pMultiAgentEnv
 from grl.rllib_tools.models.valid_actions_fcnet import get_valid_action_fcn_class_for_env
 from grl.rl_apps.centralized_critic_model import TorchCentralizedCriticModel
+
 from gym.spaces import Discrete
+
 from ray.rllib.models import ModelCatalog
+
 ModelCatalog.register_custom_model(
         "cc_model", TorchCentralizedCriticModel)
+
 def psro_tiny_bridge_ccppo_params(env: MultiAgentEnv) -> Dict[str, Any]:
     env_config={
         "version": "tiny_bridge_4p",
         "fixed_players": True,
     }
     tmp_env = TinyBridge4pMultiAgentEnv(env_config)
-    return {
-        "batch_mode": "complete_episodes",
-        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-        "num_workers": 20,
+    config = {
+        "clip_param": 0.03,
+        "entropy_coeff": 0.0001,
+        "framework": "torch",
+        "gamma": 1.0,
+        "kl_coeff": 0.2,
+        "kl_target": 0.01,
+        "lr": 0.0005,
+        "metrics_smoothing_episodes": 5000,
         "model": {
             "custom_model": "cc_model",
-            "vf_share_layers": False ### overriding the default parameter
+            "vf_share_layers": False
         },
+        "batch_mode": "complete_episodes",
+        "num_envs_per_worker": 1,
+        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "num_gpus_per_worker": 0.0,
+        "num_sgd_iter": 1,
+        "rollout_fragment_length": 256,
+        "sgd_minibatch_size": 512,
+        "train_batch_size": 2048,
+        "vf_clip_param": 10.0,
+        "vf_share_layers": False,
         "framework": "torch",
+        "num_workers": 20,
+
     }
+    return config
+
+def psro_bridge_ccppo_params(env: MultiAgentEnv) -> Dict[str, Any]:
+    env_config={
+        "version": "bridge",
+        "fixed_players": True,
+    }
+    tmp_env = TinyBridge4pMultiAgentEnv(env_config)
+    config = {
+        "clip_param": 0.03,
+        "entropy_coeff": 0.0001,
+        "framework": "torch",
+        "gamma": 1.0,
+        "kl_coeff": 0.2,
+        "kl_target": 0.01,
+        "lr": 0.0005,
+        "metrics_smoothing_episodes": 5000,
+        "model": {
+            "custom_model": "cc_model",
+            "vf_share_layers": False
+        },
+        "batch_mode": "complete_episodes",
+        "num_envs_per_worker": 1,
+        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "num_gpus_per_worker": 0.0,
+        "num_sgd_iter": 1,
+        "rollout_fragment_length": 256,
+        "sgd_minibatch_size": 512,
+        "train_batch_size": 2048,
+        "vf_clip_param": 10.0,
+        "vf_share_layers": False,
+        "framework": "torch",
+        "num_workers": 1,
+
+    }
+    return config
